5a9d48c9f49171c4e8891977a810b8d6d8affa31
Tolu Akindele <tolu@akindele.ca>
2021-04-19 10:35:02 -0700

    Display toggleable

--- src/App.js
+++ src/App.js
@@ -11,8 +11,13 @@ export default class App extends Component {
       name: '',
       email: '',
       password: '',
-      location: ''
+      location: '',
+      showForm: false
     }
+  }
+
+  toggleDisplays = () => {
+    this.setState({ showForm: !this.showForm });
   }
 
   changeHandler = (e) => {
@@ -21,9 +26,10 @@ export default class App extends Component {
 
   submitHandler = e => {
     e.preventDefault();
-    console.log(this.state);
+    const { showForm, ...formValues } = this.state
+    console.log(formValues);
 
-    axios.post('https://sheet.best/api/sheets/4dcb546c-efb2-4925-88b7-c543af060fa1', this.state)
+    axios.post('https://sheet.best/api/sheets/4dcb546c-efb2-4925-88b7-c543af060fa1', formValues)
     .then(response => {
       console.log(response);
     })
@@ -32,33 +38,45 @@ export default class App extends Component {
       name: '',
       email: '',
       password: '',
-      location: ''
+      location: '',
+      showForm: false
     });
   }
 
   render() {
-    const { name, email, password, location } = this.state;
+    const { name, email, password, location, showForm } = this.state;
     return (
       <Container fluid className="container">
         <Form className="form" onSubmit={this.submitHandler}>
           <Header as='h1'>Leftovers Sign Up</Header>
-          <Form.Field>
-            <label>Company Name</label>
-            <input placeholder='Enter your name' type="text" name = "name" value = {name} onChange={this.changeHandler}/>
-          </Form.Field>
-          <Form.Field>
-            <label>Company Email</label>
-            <input placeholder='Enter your email' type="email" name = "email" value = {email} onChange={this.changeHandler}/>
-          </Form.Field>
-          <Form.Field>
-            <label>Password</label>
-            <input placeholder='Enter your password' type="password" name = "password" value = {password} onChange={this.changeHandler}/>
-          </Form.Field>
-          <Form.Field>
-            <label>Location</label>
-            <input placeholder='Enter your location' type="text" name = "location" value = {location} onChange={this.changeHandler}/>
-          </Form.Field>
-          <Button color="blue" type='submit'>Submit</Button>
+
+          {!showForm && (
+            <div className="signup-button">
+              <Button color="green" type="button" onClick={this.toggleDisplays}>Signup</Button>
+            </div>
+          )}
+
+          {showForm && (
+            <div className="signup-form">
+              <Form.Field>
+                <label>Company Name</label>
+                <input placeholder='Enter your name' type="text" name="name" value={name} onChange={this.changeHandler}/>
+              </Form.Field>
+              <Form.Field>
+                <label>Company Email</label>
+                <input placeholder='Enter your email' type="email" name="email" value={email} onChange={this.changeHandler}/>
+              </Form.Field>
+              <Form.Field>
+                <label>Password</label>
+                <input placeholder='Enter your password' type="password" name="password" value={password} onChange={this.changeHandler}/>
+              </Form.Field>
+              <Form.Field>
+                <label>Location</label>
+                <input placeholder='Enter your location' type="text" name="location" value={location} onChange={this.changeHandler}/>
+              </Form.Field>
+              <Button color="blue" type='submit'>Submit</Button>
+            </div>
+          )}
         </Form>
       </Container>
     )
